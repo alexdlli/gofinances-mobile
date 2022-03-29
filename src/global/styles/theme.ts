cfb54a38a71acd815cd3dde4b7ba821bb5d1f65e
Alex <alex@iMac-de-Alex.Dlink>
2022-03-29 15:32:34 -0300

    Config react-native-responsive-fonts to use proportional measures

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -11,7 +11,7 @@ export default {
     attention: '#E83F5B',
     attention_light: 'rgba(232, 63, 91, 0.5)',
 
-    shape: '#353F5F',
+    shape: '#FFFFFF',
     title: '#363F5F',
     text: '#969CB2',
     background: '#F0F2F5'
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/54038618?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Alex</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
