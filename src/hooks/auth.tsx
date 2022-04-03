fa397c714e13b72b53f3fbf98a3fc76984f55d47
alex-dll <alexsandrojr.303@gmail.com>
2022-04-03 17:13:25 -0300

    Pegando os dados do usuario da conta google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 
 import * as AuthSession from "expo-auth-session";
 
@@ -8,7 +8,7 @@ interface AuthProviderProps {
 
 interface User {
   id: string;
-  user: string;
+  name: string;
   email: string;
   photo?: string;
 }
@@ -17,14 +17,17 @@ interface IAuthContextData {
   singInWhitGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: "1",
-    user: "Alex Sandro",
-    email: "alex@email.com",
-  };
+  const [user, setUser] = useState<User>({} as User);
 
   async function singInWhitGoogle() {
     try {
@@ -36,8 +39,26 @@ function AuthProvider({ children }: AuthProviderProps) {
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const response = await AuthSession.startAsync({ authUrl });
-      console.log(response);
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+      }
+
+      // console.log(response);
     } catch (error: any) {
       throw new Error(error);
     }
